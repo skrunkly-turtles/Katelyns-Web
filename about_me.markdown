fd26d2eb0b6709c41304f02ca750f96e2f2b82a4
Katelyn Wong <katelynwong100@gmail.com>
2026-05-26 15:43:36 -0400

    fixed the background image and working on about me page

--- about_me.markdown
+++
@@ -1,5 +0,0 @@
----
-layout: page
-title: "Katelyn Wong"
-permalink: /about_me/
----
--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 function hover(id, textId) {
-  document.getElementById(textId).style.fontSize = '12px';
-  document.getElementById(id).classList.add('hovered');
+  const islands = document.querySelectorAll('.islands');
+  islands.forEach(island => {
+  if (island.checkVisibility({ opacityProperty: true })) {
+    document.getElementById(textId).style.fontSize = '20px';
+    document.getElementById(textId).style.textAlign = "center";
+    document.getElementById(textId).style.color = "#94a3b8";
+    document.getElementById(textId).style.textDecoration = "none";
+    document.getElementById(id).classList.add('hovered');
+  }
+  });
 }
 
 function unhover(id, textId) {
